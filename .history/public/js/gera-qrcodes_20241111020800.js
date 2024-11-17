5c185207282675c1f0703d5a9a6066f7432dc60c
rodrigototy <56210472+rodrigototy@users.noreply.github.com>
2024-11-17 16:01:48 -0300

    Delete .history directory

--- .history/public/js/gera-qrcodes_20241111020800.js
+++
@@ -1,9 +0,0 @@
-const QRCode = require('qrcode');
-const data = require('./data/plants.json');
-
-data.forEach(plant => {
-  QRCode.toFile(`./static/qrcodes/${plant.id}.png`, `http://localhost:3000/plant/${plant.id}`, (err) => {
-    if (err) console.error(err);
-    else console.log(`QR Code gerado para ${plant.name}`);
-  });
-});
--- .history/public/js/gera-qrcodes_20241111223144.js
+++
@@ -1,9 +0,0 @@
-const QRCode = require('qrcode');
-const data = require('./data/plants.json');
-
-data.forEach(plant => {
-  QRCode.toFile(`./static/qrcodes/${plant.id}.png`, `https://horta-escolar.vercel.app//plant/${plant.id}`, (err) => {
-    if (err) console.error(err);
-    else console.log(`QR Code gerado para ${plant.name}`);
-  });
-});
